e80e52be7da6d3ea1e09998a5d8dc90b4cc01bcd
Hexa One <58103738+HexaOneOfficial@users.noreply.github.com>
2020-07-21 15:58:47 +0200

    Update buildKERNEL.sh

--- buildKERNEL.sh
+++ buildKERNEL.sh
@@ -1,1 +1,20 @@
-cp ~/builddahliagrub/dahliaos/kernel/bzImage ~/builddahliagrub/dahlia
+#!/bin/bash
+# Bash Menu Script Example
+
+PS3='Please select a kernel: '
+options=("Default 5.6.15" "5.7.9" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Default 5.6.15")
+            cp ~/builddahliagrub/dahliaos/kernel/bzImage ~/builddahliagrub/dahlia
+            ;;
+        "5.7.9")
+            cp ~/builddahliagrub/dahliaos/kernel/5.7.9/bzImage ~/builddahliagrub/dahlia
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
