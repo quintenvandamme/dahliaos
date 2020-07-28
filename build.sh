32bb8a0a69a3b42b418b7373fffabcc950d61253
Hexa One <58103738+HexaOneOfficial@users.noreply.github.com>
2020-07-28 13:07:18 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -25,3 +25,6 @@ cp -r ~/builddahliagrub/dahliaos/grub ~/builddahliagrub/boot
 cd 
 cd ~/builddahliagrub/boot/grub/x86_64-efi
 unzip x86_64-efi.zip
+cp ~/builddahliagrub/dahliaos/buildKERNEL.sh ~/
+cp ~/builddahliagrub/dahliaos/buildIMAGE.sh ~/
+
