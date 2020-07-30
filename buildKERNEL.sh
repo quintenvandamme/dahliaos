16847c09d5f6ab7b8199cc9927d4320adbb9cef4
Hexa One <58103738+HexaOneOfficial@users.noreply.github.com>
2020-07-30 11:58:07 +0200

    update 5.7.11

--- buildKERNEL.sh
+++ buildKERNEL.sh
@@ -8,9 +8,10 @@ TITLE="buildKERNEL"
 MENU="Choose your kernel:"
 
 OPTIONS=(1 "Default 5.6.15"
-         2 "latest 5.7.10"
-         3 "rc 5.8.0-rc7"
-         4 "5.7.9")
+         2 "latest 5.7.11"
+         3 "5.7.10")
+         4 "5.7.9"
+         5 "rc 5.8.0-rc7")
 CHOICE=$(dialog --clear \
                 --backtitle "$BACKTITLE" \
                 --title "$TITLE" \
@@ -26,10 +27,23 @@ case $CHOICE in
             ;;
         2)
             cd
+            wget "https://github.com/HexaOneOfficial/kerneldahlia/releases/download/5.7.11/linux5.7.11.zip"
+            cp linux5.7.11zip ~/builddahliagrub/dahlia
+            cd ~/builddahliagrub/dahlia
+            unzip linux5.7.11.zip
+            rm BUILD
+            rm COPYING
+            rm linux5.7.11.zip
+            ;;    
+        3)
+            cd
             wget "https://github.com/HexaOneOfficial/kerneldahlia/releases/download/5.7.10/bzImage"
             cp ~/bzImage ~/builddahliagrub/dahlia
             ;;
-        3)
+        4)
+            cp ~/builddahliagrub/dahliaos/kernel/5.7.9/bzImage ~/builddahliagrub/dahlia
+            ;;
+        5)
             cd
             wget "https://github.com/HexaOneOfficial/kerneldahlia/releases/download/5.8.0rc7/linux5.8.0-rc7.zip"
             cp linux5.8.0-rc7.zip ~/builddahliagrub/dahlia
@@ -38,8 +52,5 @@ case $CHOICE in
             rm BUILD
             rm COPYING
             rm linux5.8.0-rc7.zip
-            ;;
-        4)
-            cp ~/builddahliagrub/dahliaos/kernel/5.7.9/bzImage ~/builddahliagrub/dahlia
-            ;;
+            ;;    
 esac
