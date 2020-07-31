e5a22469aeeacec72f918b4227a802e49ba5d1d3
Hexa One <58103738+HexaOneOfficial@users.noreply.github.com>
2020-07-31 21:10:43 +0200

    add 5.7.12

--- buildKERNEL.sh
+++ buildKERNEL.sh
@@ -2,16 +2,17 @@
 
 HEIGHT=20
 WIDTH=40
-CHOICE_HEIGHT=6
+CHOICE_HEIGHT=7
 BACKTITLE="buildKERNEL"
 TITLE="buildKERNEL"
 MENU="Choose your kernel:"
 
 OPTIONS=(1 "Default 5.6.15"
-         2 "latest 5.7.11"
-         3 "5.7.10"
-         4 "5.7.9"
-         5 "rc 5.8.0-rc7")
+         2 "latest 5.7.12"
+         3 "5.7.11"
+         4 "5.7.10"
+         5 "5.7.9"
+         6 "rc 5.8.0-rc7")
 CHOICE=$(dialog --clear \
                 --backtitle "$BACKTITLE" \
                 --title "$TITLE" \
@@ -27,6 +28,16 @@ case $CHOICE in
             ;;
         2)
             cd
+            wget "https://github.com/HexaOneOfficial/kerneldahlia/releases/download/5.7.12/linux5.7.12.zip"
+            cp linux5.7.11zip ~/builddahliagrub/dahlia
+            cd ~/builddahliagrub/dahlia
+            unzip linux5.7.12.zip
+            rm BUILD
+            rm COPYING
+            rm linux5.7.12.zip
+            ;;   
+        3)
+            cd
             wget "https://github.com/HexaOneOfficial/kerneldahlia/releases/download/5.7.11/linux5.7.11.zip"
             cp linux5.7.11zip ~/builddahliagrub/dahlia
             cd ~/builddahliagrub/dahlia
@@ -34,16 +45,16 @@ case $CHOICE in
             rm BUILD
             rm COPYING
             rm linux5.7.11.zip
-            ;;    
-        3)
+            ;;   
+        4)
             cd
             wget "https://github.com/HexaOneOfficial/kerneldahlia/releases/download/5.7.10/bzImage"
             cp ~/bzImage ~/builddahliagrub/dahlia
             ;;
-        4)
-            cp ~/builddahliagrub/dahliaos/kernel/5.7.9/bzImage ~/builddahliagrub/dahlia
-            ;;
         5)
+            cp ~/builddahliagrub/dahliaos/kernel/5.7.9/bzImage ~/builddahliagrub/dahlia
+            ;;  
+            6)
             cd
             wget "https://github.com/HexaOneOfficial/kerneldahlia/releases/download/5.8.0rc7/linux5.8.0-rc7.zip"
             cp linux5.8.0-rc7.zip ~/builddahliagrub/dahlia
@@ -52,5 +63,5 @@ case $CHOICE in
             rm BUILD
             rm COPYING
             rm linux5.8.0-rc7.zip
-            ;;    
+            ;;
 esac
