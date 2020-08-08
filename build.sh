e61bdf9b34abbb3b7b01aec732e4bb707a3bdbda
Hexa One <58103738+HexaOneOfficial@users.noreply.github.com>
2020-08-08 16:08:23 +0200

    fix *not found

--- build.sh
+++ build.sh
@@ -24,6 +24,7 @@ case $CHOICE in
             echo test
             ;;
         2)
+            git clone https://github.com/HexaOneOfficial/dahliaos.git
             mkdir ~/builddahliagrub
             cd 
             cp -r ~/dahliaos ~/builddahliagrub
@@ -65,5 +66,4 @@ case $CHOICE in
 
 
 esac
-
 
